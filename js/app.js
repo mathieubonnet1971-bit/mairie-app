57e6a86fa6003b0e47c215971346731b3a5af2e2
mathieubonnet1971-bit <mathieu.bonnet.1971@gmail.com>
2026-04-29 13:43:56 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(() => console.log('SW enregistré'))
-    .catch(err => console.log('SW erreur:', err));
-}
+window.addEventListener('load', () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(() => console.log('SW OK'))
+      .catch(err => console.log('SW erreur:', err));
+  }
+});
